8739e0f5763832374fd069deb5a02ca168f346ba
Aastha Kumari <aasthakumari20002@gmail.com>
2026-02-12 16:21:52 +0530

    Time: 91 ms (5.11%) | Memory: 65 MB (6.72%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -5,15 +5,23 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack =[]
-        curr = head
-        while curr:
-            stack.append(curr.val)
-            curr= curr.next
-        curr = head
-        while curr:
-            if curr.val!= stack.pop():
-                return False
-            curr=curr.next
-        return True          
+        self.left=ListNode(0, head)
+        def recursiveFunc(head):
+            if not head:
+                return True
+            right = recursiveFunc(head.next)
+            self.left = self.left.next
+            return right and self.left.val== head.val
+        return recursiveFunc(head)        
+        # stack =[]
+        # curr = head
+        # while curr:
+        #     stack.append(curr.val)
+        #     curr= curr.next
+        # curr = head
+        # while curr:
+        #     if curr.val!= stack.pop():
+        #         return False
+        #     curr=curr.next
+        # return True          
         
