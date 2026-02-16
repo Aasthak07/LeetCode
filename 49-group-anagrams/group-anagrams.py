3f353d73d8916f1c71faa0a9172311766ff7336e
Aastha Kumari <aasthakumari20002@gmail.com>
2026-02-16 22:10:27 +0530

    Time: 9 ms (89.93%) | Memory: 22.1 MB (57.91%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,19 +1,11 @@
 class Solution:
-    def sortString(self, s):
-        s1= list(s)
-        s1.sort()
-        return "".join(s1)
-
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        dict1={}
-        for s in strs:
-            key= self.sortString(s)
-            if key in dict1:
-                dict1[key].append(s)
-            else:
-                dict1[key] = [s]
-
-        return list(dict1.values())           
-
-
+        from collections import defaultdict
         
+        groups = defaultdict(list)
+        
+        for word in strs:
+            key = ''.join(sorted(word))   # sorted word as key
+            groups[key].append(word)
+        
+        return list(groups.values())
