b4967d6e276e3e505d41130a5dec536b1a7db84c
Aastha Kumari <aasthakumari20002@gmail.com>
2026-04-08 18:47:11 +0530

    Time: 4 ms (28.74%) | Memory: 19.3 MB (78.30%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,32 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], arr: List[int]) -> List[int]:
+
+        n= len(arr)
+        # ans= [0]*n for all values
+        ans={}
+        st=[]
+
+        for i in range(n-1, -1, -1):
+            while len(st)>0 and st[-1]<= arr[i]:
+                st.pop()
+            if len(st)==0:
+                # ans[i]=-1
+                ans[arr[i]]=-1
+
+            else:
+                # ans[i] = st[-1]  
+                ans[arr[i]] = st[-1]
+                  
+
+            st.append(arr[i])
+
+        res= []
+        for i in nums1:
+            res.append(ans[i])
+
+        return res            
+
+
+
+
+        
