92894f41131a6a6e2d4d1a34bea3e77d73a4cfd3
Aastha Kumari <aasthakumari20002@gmail.com>
2026-04-14 23:11:50 +0530

    Time: 15 ms (89.00%) | Memory: 20.7 MB (71.02%) - LeetSync

---
+++ 697-degree-of-an-array/degree-of-an-array.py
@@ -0,0 +1,23 @@
+class Solution:
+    def findShortestSubArray(self, nums: List[int]) -> int:
+        freq = {}
+        first = {}
+        last = {}
+
+        for i, num in enumerate(nums):
+            if num not in first:
+                first[num] = i
+            
+            last[num] = i
+            freq[num] = freq.get(num, 0) + 1
+
+        degree = max(freq.values())
+
+        min_len = float('inf')
+
+        for num in freq:
+            if freq[num] == degree:
+                length = last[num] - first[num] + 1
+                min_len = min(min_len, length)
+
+        return min_len
